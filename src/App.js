daff29dba6d0d707a66fac94def37e95fb357378
juliasriera <juliasriera@gmail.com>
2021-04-19 17:58:35 +0200

    button

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
-export default () => {
 
+import React, { useEffect, useState } from 'react';
+
+const App = () => {
+    const [joke, setJoke] = useState(null);
+ 
+    useEffect(() => {
+      fetch('https://icanhazdadjoke.com/', {  
+        headers: {
+
+            accept: "application/json"
+        },
+
+    })
+      .then(res => res.json()) 
+      .then(data => setJoke(data.joke)); 
+    }, []); 
+
+    
+//<button onClick={() => {alert("Hello!")}}>Siguiente</button>
+    
     return (
-        <div>
-            hello World!
-        </div>
-    );
-};
+      <div>
+      <h1>ACUDITS</h1>
+    
+    <button onClick={() => {alert({"Hello!"})}}>Siguiente</button>
+    </div>
+    )
+  }
+
+export default App
